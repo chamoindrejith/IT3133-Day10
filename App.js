cb7c377c82cafc9948536f7e43f1521773f03f37
Chamodi Indrejith <chamodiindrejith14@gmail.com>
2024-12-31 14:50:15 +0530

    feat: enabling react navigations

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Home from './components/Home';
+import ContactUs from './components/ContactUs';
 
 const Stack = createNativeStackNavigator();
-export default function App() {
-
+function RootStack() {
   return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#9370db'},
+      }}
+      >
+      <Stack.Screen name="Home" component={Home}  options={{ title: 'Welcome' }}/>
+      <Stack.Screen name="ContactUs" component={ContactUs}  options={{ title: 'Contact Us' }} />
+    </Stack.Navigator>
+  );
+}
 
+export default function App() {
+  return (
     <SafeAreaProvider>
-      
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
     </SafeAreaProvider>
 
   );
